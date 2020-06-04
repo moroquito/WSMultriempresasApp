7f5eb06575088f9e6bc12ac26aa27647c306859f
Javier Cabrera Sánchez <jecabrerasanchez@hotmail.com>
2020-06-04 09:56:01 -0600

    mensaje 2

---
+++ ConsoleAppjecs/ConsoleAppjecs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppjecs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
